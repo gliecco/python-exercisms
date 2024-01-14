2539393d3032e96bec894be445d2e26493df5d3b
gliecco <vapour4300@gmail.com>
2024-01-14 11:53:21 -0300

    remove irrelevant text

--- lasagna/lasagna.py
+++ lasagna/lasagna.py
@@ -1,8 +1,5 @@
 """Functions used in preparing Guido's gorgeous lasagna.
 
-Learn about Guido, the creator of the Python language:
-https://en.wikipedia.org/wiki/Guido_van_Rossum
-
 This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
